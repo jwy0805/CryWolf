e704c4cb79c918d22878ffeec2323446b8961389
Adam-WY-Jeong <jwy0805@gmail.com>
2023-02-11 17:30:15 +0900

    sunblossom update

--- Scripts/Controllers/TowerController/SunBlossomController.cs
+++ Scripts/Controllers/TowerController/SunBlossomController.cs
@@ -1,18 +1,77 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SunBlossomController : MonoBehaviour
+public class SunBlossomController : TowerController
 {
-    // Start is called before the first frame update
-    void Start()
+    private float _mpTime = 1f;
+    private float _lastMpTime = 0f;
+    private bool _heal = false;
+    private bool _health = false;
+    private bool _slow = false;
+    private bool _slowAttack = false;
+    
+    protected override string NewSkill
+    {
+        get => _newSkill;
+        set
+        {
+            _newSkill = value;
+            Define.Skill skill = (Define.Skill)Enum.Parse(typeof(Define.Skill), _newSkill);
+
+            switch (skill)
+            {
+                case Define.Skill.SunBlossomHeal:
+                    _heal = true;
+                    break;
+                case Define.Skill.SunBlossomHealth:
+                    _health = true;
+                    break;
+                case Define.Skill.SunBlossomSlow:
+                    _slow = true;
+                    break;
+                case Define.Skill.SunBlossomSlowAttack:
+                    _slowAttack = true;
+                    break;
+            }
+        }
+    }
+    
+    protected override void Init()
+    {
+        base.Init();
+
+        _stat.Hp = 80;
+        _stat.MaxHp = 80;
+        _stat.Mp = 0;
+        _stat.maxMp = 40;
+        _stat.Defense = 0;
+        _stat.AttackRange = 10;
+
+        SkillInit();
+    }
+
+    protected override void UpdateIdle()
+    {
+        if (Time.deltaTime > _lastMpTime + _mpTime)
+        {
+            _stat.Mp += 4;
+        }
+
+        if (_stat.Mp >= _stat.maxMp)
+        {
+            State = Define.State.Skill;
+        }
+    }
+
+    private void OnSkillEvent()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void OnEndEvent()
     {
-        
+        State = Define.State.Idle;
     }
 }
